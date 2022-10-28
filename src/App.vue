87312aa83d3a547cfb58e2c068cd20916de4711d
moneeb <moneebalalfi@gmail.com>
2022-10-29 00:51:52 +0300

    refactor: adding TodoList component

--- src/App.vue
+++ src/App.vue
@@ -4,54 +4,23 @@
       <h1>Vuejs3 Todo Application</h1>
     </header>
 
-    <CreateTodo todoContent="todoContent" @onAddTodo="addNewTodo" />
+    <CreateTodo :todoContent="todoContent" @onAddTodo="addNewTodo" />
 
-    <section class="todo-list" v-if="todos.length > 0">
-      <h2 class="title">Todo List</h2>
-      <div
-        v-for="(todo, i) in todos"
-        :key="todo.id"
-        class="todo-item"
-        :class="{ done: todo.isDone }"
-      >
-        <h3 class="todo-content">{{ todo.content }}</h3>
-        <div class="todo-item-controls">
-          <button class="toggle-done-btn" @click="toggleTodoState(todo)">
-            Done {{ todo.isDone ? "✓" : "" }}
-          </button>
-          <button class="remove-btn" @click="removeTodo(i)">Remove</button>
-        </div>
-      </div>
-      <div class="todo-list-controls">
-        <p>
-          Pending <span>{{ pendingTodos }}</span>
-        </p>
-        <button
-          class="set-all-done-btn"
-          v-if="pendingTodos !== 0"
-          @click="markAllDone"
-        >
-          Set All Done
-        </button>
-      </div>
-    </section>
+    <TodoList :data="todos" />
   </main>
 </template>
 
 <script>
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import CreateTodo from "./components/CreateTodo.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
   name: "App",
-  components: { CreateTodo },
+  components: { CreateTodo, TodoList },
   setup() {
     const todoContent = ref("");
     const todos = ref([]);
-
-    const pendingTodos = computed(() => {
-      return todos.value.filter((todo) => !todo.isDone).length;
-    });
 
     function addNewTodo(data) {
       const { textareaRef, todoContent } = data;
@@ -69,20 +38,10 @@ export default {
       todoContent.value = "";
     }
 
-    const toggleTodoState = (todo) => (todo.isDone = !todo.isDone);
-    const removeTodo = (index) => todos.value.splice(index, 1);
-    const markAllDone = () =>
-      todos.value.forEach((todo) => (todo.isDone = true));
-
     return {
       todos,
       addNewTodo,
       todoContent,
-      toggleTodoState,
-      removeTodo,
-      markAllDone,
-
-      pendingTodos,
     };
   },
 };
---
+++ src/components/TodoList.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="todo-list" v-if="todos.length > 0">
+    <h2 class="title">Todo List</h2>
+    <div
+      v-for="(todo, i) in todos"
+      :key="todo.id"
+      class="todo-item"
+      :class="{ done: todo.isDone }"
+    >
+      <h3 class="todo-content">{{ todo.content }}</h3>
+      <div class="todo-item-controls">
+        <button class="toggle-done-btn" @click="toggleTodoState(todo)">
+          Done {{ todo.isDone ? "✓" : "" }}
+        </button>
+        <button class="remove-btn" @click="removeTodo(i)">Remove</button>
+      </div>
+    </div>
+    <div class="todo-list-controls">
+      <p>
+        Pending <span>{{ pendingTodos }}</span>
+      </p>
+      <button
+        class="set-all-done-btn"
+        v-if="pendingTodos !== 0"
+        @click="markAllDone"
+      >
+        Set All Done
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+
+export default {
+  props: ["data"],
+
+  setup(props) {
+    const todos = ref(props.data);
+
+    const toggleTodoState = (todo) => (todo.isDone = !todo.isDone);
+    const removeTodo = (index) => todos.value.splice(index, 1);
+
+    const pendingTodos = computed(() => {
+      return todos.value.filter((todo) => !todo.isDone).length;
+    });
+
+    const markAllDone = () =>
+      todos.value.forEach((todo) => (todo.isDone = true));
+
+    return {
+      todos,
+      toggleTodoState,
+      removeTodo,
+      pendingTodos,
+      markAllDone,
+    };
+  },
+};
+</script>
