20727763702f2485f8731fc9cb9b176c748e980f
moneeb <moneebalalfi@gmail.com>
2022-10-28 20:47:09 +0300

    feat: adding the basic app's logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <header>
+      <h1>Vuejs3 Todo Application</h1>
+    </header>
+    <section class="create-todo">
+      <form @submit.prevent="addNewTodo">
+        <label for="new-todo">New Todo</label>
+        <input id="new-todo" v-model="todoContent" />
+        <button>Add</button>
+      </form>
+      <button @click="markAllDone">Mark all done</button>
+    </section>
+    <section class="todo-list">
+      <div
+        v-for="(todo, i) in todos"
+        :key="todo.id"
+        @click="toggleTodoState(todo)"
+        :class="{ done: todo.isDone }"
+      >
+        <h3>{{ todo.content }}</h3>
+        <button @click="removeTodo(i)">Remove</button>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  components: {},
+  setup() {
+    const todoContent = ref("");
+    const todos = ref([]);
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    const addNewTodo = () => {
+      todos.value.push({
+        id: 0,
+        content: todoContent.value,
+        isDone: !true,
+      });
+      todoContent.value = "";
+    };
+
+    const toggleTodoState = (todo) => (todo.isDone = !todo.isDone);
+    const removeTodo = (index) => todos.value.splice(index, 1);
+    const markAllDone = () =>
+      todos.value.forEach((todo) => (todo.isDone = true));
+
+    return {
+      todos,
+      addNewTodo,
+      todoContent,
+      toggleTodoState,
+      removeTodo,
+      markAllDone,
+    };
+  },
+};
+</script>
